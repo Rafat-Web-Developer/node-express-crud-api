9e51db1f271142db8a1dbfb89a98c59be006a944
Tareq Rana <rafat.webdeveloper@gmail.com>
2022-09-21 16:21:12 +0600

    Add version

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const usersRouter = require("./routes/users-route");
+const usersRouter = require("./routes/v1/users-route");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.use(usersRouter);
+app.use("/api/v1/users", usersRouter);
 
 app.listen(port);
