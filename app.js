7474e5f77bf2e741697c63e7d998bffc2bb7ef03
Tareq Rana <rafat.webdeveloper@gmail.com>
2022-09-21 17:40:15 +0600

    Handling known route error

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const HttpError = require("./models/http-error");
 const usersRouter = require("./routes/v1/users-route");
 
 const app = express();
@@ -7,6 +8,11 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use("/api/v1/users", usersRouter);
+
+app.use((req, res, next) => {
+  const error = new HttpError("Route not found", 404);
+  throw error;
+});
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
