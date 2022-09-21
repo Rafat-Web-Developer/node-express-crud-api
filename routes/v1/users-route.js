bf95c23bd7d41c7e33b3a75b3511337391307802
Tareq Rana <rafat.webdeveloper@gmail.com>
2022-09-21 16:26:36 +0600

    Find single user

--- routes/v1/users-route.js
+++ routes/v1/users-route.js
@@ -2,8 +2,21 @@ const express = require("express");
 
 const router = express.Router();
 
+const DUMMY_USERS = [
+  {
+    id: 1,
+    name: "Rafat Hossain",
+  },
+];
+
 router.get("/", (req, res) => {
-  res.json({ name: "User" });
+  res.json(DUMMY_USERS);
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const user = DUMMY_USERS.find((user) => user.id === Number(id));
+  res.json(user);
 });
 
 module.exports = router;
