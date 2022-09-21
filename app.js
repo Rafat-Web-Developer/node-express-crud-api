7e78f2bc9cac733920e61b6b4b4ef8b77b6f9140
Tareq Rana <rafat.webdeveloper@gmail.com>
2022-09-21 16:56:15 +0600

    Handling error

--- app.js
+++ app.js
@@ -8,4 +8,12 @@ app.use(express.json());
 
 app.use("/api/v1/users", usersRouter);
 
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error" });
+});
+
 app.listen(port);
--- routes/v1/users-route.js
+++ routes/v1/users-route.js
@@ -13,9 +13,14 @@ router.get("/", (req, res) => {
   res.json({ data: DUMMY_USERS });
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", (req, res, next) => {
   const { id } = req.params;
   const user = DUMMY_USERS.find((user) => user.id === Number(id));
+  if (!user) {
+    const error = new Error("Could not find that user");
+    error.code = 404;
+    return next(error);
+  }
   res.json({ data: user });
 });
 
